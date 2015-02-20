4b337ddbd0b5c3854fcdf34c75e9acb72a0dc6ea
TakaoHoshino <tk.1987.6.9@gmail.com>
2015-02-20 13:26:16 +0800

    Initial Commit

---
+++ memorizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  memorizer
+//
+//  Created by 星野嵩夫 on 2015/02/20.
+//  Copyright (c) 2015年 星野嵩夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ memorizerTests/memorizerTests.m
@@ -0,0 +1,40 @@
+//
+//  memorizerTests.m
+//  memorizerTests
+//
+//  Created by 星野嵩夫 on 2015/02/20.
+//  Copyright (c) 2015年 星野嵩夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface memorizerTests : XCTestCase
+
+@end
+
+@implementation memorizerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
